5d0110a6000a0ce38856f383647f62c4c0ce9e59
goncalofm90 <goncalofm90@gmail.com>
2021-04-02 13:57:13 +0100

    connect to db :card_file_box:

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
-
+connectDB();
 app.get("/", (req, res) => res.send("api running"));
 
 const PORT = process.env.PORT || 5000;
