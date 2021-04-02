1b4c1bcf93eb934f9d24a3112aa842f06d2fb462
goncalofm90 <goncalofm90@gmail.com>
2021-04-02 16:39:38 +0100

    authentication & token verify 🌐

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../../models/User");
+const auth = require("../../middleware/auth");
 
 // GET api/auth
 // public
-router.get("/", (req, res) => res.send("Auth route"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    res.status(500).send("Server error.");
+  }
+});
 
 module.exports = router;
