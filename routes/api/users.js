eb6bde5d1f310b0d4cc4a14d7455d7b657c639a1
goncalofm90 <goncalofm90@gmail.com>
2021-04-02 16:18:49 +0100

    implementing bcryp and json web token :rotating_light:

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const config = require("config");
 const router = express.Router();
 const { check, validationResult } = require("express-validator/check");
+const gravatar = require("gravatar");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const User = require("../../models/User");
 
 // POST api/users
 router.post(
@@ -14,13 +19,58 @@ router.post(
     }),
   ],
   //requests
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
-    } else {
     }
-    res.send("user request sent");
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists." }] });
+      }
+      const avatar = gravatar.url(email, {
+        //default size
+        s: "200",
+        //rating
+        r: "pg",
+        //default
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+      //encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
