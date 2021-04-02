67371fd98d0d07f032d850cff6b2ff985d8a383c
goncalofm90 <goncalofm90@gmail.com>
2021-04-02 14:10:42 +0100

    .

---
+++ routes/api/profile.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router;
+
+// GET api/profile
+// public
+router.get("/", (req, res) => res.send("profile route"));
+
+module.exports = router;
