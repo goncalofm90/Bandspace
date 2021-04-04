0e866894a5917a9c9c586f03bf2e6a97923053d9
goncalofm90 <goncalofm90@gmail.com>
2021-04-04 17:20:25 +0100

    starting the front-end :art:

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React from "react";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Bandspace</h1>
-    </div>
+    <>
+      <Navbar />
+      <Landing />
+    </>
   );
-}
+};
 
 export default App;
