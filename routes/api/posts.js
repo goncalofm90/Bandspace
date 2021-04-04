dba3c5062a029b87ca9dbab8d805d7af7afdd12a
goncalofm90 <goncalofm90@gmail.com>
2021-04-04 12:48:40 +0100

    implementing post route :truck:

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,8 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const Profile = require("../../models/Profile");
+const Post = require("../../models/Post");
 
-// GET api/posts
-// public
-router.get("/", (req, res) => res.send("posts route"));
+// Post api/posts
+// create post
+router.post(
+  "/",
+  [auth, [check("text", "Text is required.").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+      res.json(post);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500);
+    }
+  }
+);
 
 module.exports = router;
