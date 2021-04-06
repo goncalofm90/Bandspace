9711781e353ee63dede1001dc7a42dc54031784e
goncalofm90 <goncalofm90@gmail.com>
2021-04-06 14:32:58 +0100

    adding form layouts :art:

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>login</div>;
-};
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
+  const { email, password } = formData;
+
+  const handleChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("success");
+  };
+  return (
+    <>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Login
+      </p>
+      <form className="form" onSubmit={(e) => handleSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            autoComplete="on"
+            value={password}
+            onChange={(e) => handleChange(e)}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1" /> Don't have an account ?{" "}
+      <Link to="/register">Sign up</Link>
+    </>
+  );
+};
 export default Login;
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,41 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const Register = () => (
-  <>
-    <h1 className="large text-primary">Sign Up</h1>
-    <p className="lead">
-      <i className="fas fa-user"></i> Create Your Account
-    </p>
-    <form className="form" action="create-profile.html">
-      <div className="form-group">
-        <input type="text" placeholder="Name" name="name" required />
-      </div>
-      <div className="form-group">
-        <input type="email" placeholder="Email Address" name="email" />
-        <small className="form-text">
-          This site uses Gravatar so if you want a profile image, use a Gravatar
-          email
-        </small>
-      </div>
-      <div className="form-group">
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          minLength="6"
-        />
-      </div>
-      <div className="form-group">
-        <input
-          type="password"
-          placeholder="Confirm Password"
-          name="password2"
-          minLength="6"
-        />
-      </div>
-      <input type="submit" className="btn btn-primary" value="Register" />
-    </form>
-    <p className="my-1" />
-  </>
-);
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const handleChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("passwords dont match");
+    } else {
+      console.log("sucess");
+    }
+  };
+  return (
+    <>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Create Your Account
+      </p>
+      <form className="form" onSubmit={(e) => handleSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={(e) => handleChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => handleChange(e)}
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            autoComplete="on"
+            value={password}
+            onChange={(e) => handleChange(e)}
+            minLength="6"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            value={password2}
+            autoComplete="on"
+            onChange={(e) => handleChange(e)}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1" /> Already have an account ?{" "}
+      <Link to="/login">Sign In</Link>
+    </>
+  );
+};
 export default Register;
