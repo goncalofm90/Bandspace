6d04655bd99686f52f946c68567483f344eebb7a
goncalofm90 <goncalofm90@gmail.com>
2025-03-26 22:13:03 +0000

    fix routes (old project)

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
       setAuthToken(localStorage.token);
     }
     store.dispatch(loadUser());
-
+    
     // log user out from all tabs if they log out in one tab
     window.addEventListener("storage", () => {
       if (!localStorage.token) store.dispatch({ type: LOGOUT });
@@ -40,43 +40,47 @@ const App = () => {
   return (
     <Provider store={store}>
       <Router>
-        <>
+        <div className="App">
           <Navbar />
-          <Route exact path="/" component={Landing} />
-          <section className="container">
-            <Alert />
-            <Switch>
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/profiles" component={Profiles} />
-              <Route exact path="/profile/:id" component={Profile} />
-              <PrivateRoute exact path="/dashboard" component={Dashboard} />
-              <PrivateRoute
-                exact
-                path="/create-profile"
-                component={CreateProfile}
-              />
-              <PrivateRoute
-                exact
-                path="/edit-profile"
-                component={EditProfile}
-              />
-              <PrivateRoute
-                exact
-                path="/add-experience"
-                component={AddExperience}
-              />
-              <PrivateRoute
-                exact
-                path="/add-education"
-                component={AddEducation}
-              />
-              <PrivateRoute exact path="/posts" component={Posts} />
-              <PrivateRoute exact path="/posts/:id" component={Post} />
-              <Route component={NotFound} />
-            </Switch>
-          </section>
-        </>
+          <Alert />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route>
+              <section className="container">
+                <Switch>
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                  <Route exact path="/profiles" component={Profiles} />
+                  <Route exact path="/profile/:id" component={Profile} />
+                  <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                  <PrivateRoute
+                    exact
+                    path="/create-profile"
+                    component={CreateProfile}
+                  />
+                  <PrivateRoute
+                    exact
+                    path="/edit-profile"
+                    component={EditProfile}
+                  />
+                  <PrivateRoute
+                    exact
+                    path="/add-experience"
+                    component={AddExperience}
+                  />
+                  <PrivateRoute
+                    exact
+                    path="/add-education"
+                    component={AddEducation}
+                  />
+                  <PrivateRoute exact path="/posts" component={Posts} />
+                  <PrivateRoute exact path="/posts/:id" component={Post} />
+                  <Route component={NotFound} />
+                </Switch>
+              </section>
+            </Route>
+          </Switch>
+        </div>
       </Router>
     </Provider>
   );
